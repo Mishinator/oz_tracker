0c648a0e7928d05a84da82a4ffc7d1a7572a3d14
m.neviarouski <m.neviarouski@sumatosoft.com>
2024-10-01 01:56:57 +0300

    Upd db config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,9 @@
 Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://localhost:6379/0' }
-    config.logger = Logger.new(Rails.root.join('log', 'sidekiq.log'))
-  end
-  
-  Sidekiq.configure_client do |config|
-    config.redis = { url: 'redis://localhost:6379/0' }
-    config.logger = Logger.new(Rails.root.join('log', 'sidekiq.log'))
-  end
-  
+  config.redis = { url: ENV['REDIS_URL'] || 'redis://localhost:6379/0' }
+  config.logger = Logger.new(Rails.root.join('log', 'sidekiq.log'))
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['REDIS_URL'] || 'redis://localhost:6379/0' }
+  config.logger = Logger.new(Rails.root.join('log', 'sidekiq.log'))
+end
